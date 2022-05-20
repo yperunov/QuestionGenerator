9baa900bac2687c2b0edbe6689c0bbff9e5a0c6c
yperunov <perunovyuri@gmail.com>
2022-05-21 01:03:05 +0300

    Initial commit

---
+++ src/test/java/dev/ji/questiongenerator/QuestionGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ji.questiongenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
